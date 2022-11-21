ff9a217d9d893c068bba09a9f3c8325836188b69
ZnZ Botz <71170819+FriskaNaila@users.noreply.github.com>
2022-11-21 10:37:43 +0700

    Update owner-setbotpp.js

--- plugins/owner-setbotpp.js
+++ plugins/owner-setbotpp.js
@@ -1,22 +1,19 @@
-/*
-   Silahkan Di Pakek
-   Tapi Bantu Rapihin :v
-   Buatan: Miaweers
-*/
-
-let handler = async (m, { conn, args }) => {
-    let bot = conn.user.jid // Bot
+let handler = async (m, { conn, usedPrefix, command }) => {
+    let bot = conn.user.jid
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || ''
     if (/image/.test(mime)) {
-      let img = await q.download()
-      if (!img) throw `Foto tidak ditemukan`
-     conn.updateProfilePicture (bot, img)
-    conn.reply(m.chat, 'Sukses Mengganti Foto Profile Bot!', m)
-	}
-    }
-handler.help = ['setbotpp']
-handler.command = /^(setbotpp)$/i
+        let img = await q.download()
+        if (!img) throw 'Gambar tidak ditemukan!'
+        await conn.updateProfilePicture(bot, img)
+        conn.reply(m.chat, 'Sukses Mengganti Foto Profile Bot!', m)
+    } else throw `kirim/Reply gambar dengan caption *${usedPrefix + command}*`
+}
+handler.help = ['setppbot']
+handler.tags = ['owner']
+
+handler.command = /^setppbot$/i
+
 handler.owner = true
 
 module.exports = handler
